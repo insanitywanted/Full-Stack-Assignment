b57220abf1b7acac9aa02dfb238e03a954362674
Code Quant <12227706+code-quant@users.noreply.github.com>
2020-03-20 10:19:14 +0530

    Add insertion SQL query

---
+++ Problem 1/add.php
@@ -0,0 +1,26 @@
+<?php include('config.php'); ?>
+
+<?php
+
+    if (isset($_POST['bttn']))
+    {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $contact = $_POST['contact'];
+        $email = $_POST['email'];
+
+        $sql = "INSERT INTO `users` (id, name, age, contact, email) VALUES('$id', '$name', '$age', '$contact', '$email');";
+        
+        if(mysqli_query($con,$sql))
+        {
+            echo "Insertion Sucessful!";
+        }
+        else 
+            echo "Insertion Unsuccesful!";
+    }
+    else
+    {
+        echo "Hi! Welcome Guest";
+    }
+?>
