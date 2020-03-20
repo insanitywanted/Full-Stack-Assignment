1abdc8c701161ccc746b0cddc263b825488a473c
Code Quant <12227706+code-quant@users.noreply.github.com>
2020-03-20 16:40:03 +0530

    Add bootstrap stylesheet

--- Problem 1/form.php
+++ Problem 1/form.php
@@ -9,14 +9,13 @@
         echo $_POST['email'].'<br>';
     }
     else
-    {
         echo "Hi! Welcome User";
-    }
 ?>
 <html>
     <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>
-        Insert Record
+        Insert Record!
         </title>
     </head>
 
@@ -25,26 +24,26 @@
             <table>
                 <tr>
                 <td>ID</td>
-                <td><input type="text" name="id" value=""></td>
+                <td><input type="text" class="form-control" name="id" value=""></td>
                 </tr>
                 <tr>
                     <td>Name</td>
-                    <td><input type="text" name="name" value=""></td>
+                    <td><input type="text" class="form-control" name="name" value=""></td>
                 </tr>
                 <tr>
                     <td>Age</td>
-                    <td><input type="text" name="age" value=""></td>
+                    <td><input type="text" class="form-control" name="age" value=""></td>
                 </tr>
                 <tr>
                     <td>Contact</td>
-                    <td><input type="text" name="contact" value=""></td>
+                    <td><input type="text" class="form-control" name="contact" value=""></td>
                 </tr>
                 <tr>
                     <td>Email</td>
-                    <td><input type="text" name="email" value=""></td>
+                    <td><input type="text" class="form-control" name="email" value=""></td>
                 </tr>
                 <tr>
-                    <td><input type="submit" value="Submit" name='btn'></td>
+                    <td><br><input type="submit" class="btn btn-primary" value="Submit" name='btn'></td>
                 </tr>
             </table>
         </form> 
